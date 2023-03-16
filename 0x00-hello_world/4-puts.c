059b99f3c59a080013038755d37d5e2fd2bc4c35
Dinar-Tsegazeab <dinaratatsegazeab@gmail.com>
2023-03-16 03:11:00 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * A C program that prints a message with the function puts
+ * Return: 0
+ */
+int main(viod)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
