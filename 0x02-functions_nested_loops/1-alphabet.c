6fab99723c55931d3ebc791d5cc611bf21127ecc
Dinar-Tsegazeab <dinaratatsegazeab@gmail.com>
2023-03-21 14:22:10 -0700

    I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet 
+ * 
+ * Return:void
+ */
+void print_alphabet(void)
+{
+char txt = 'a';
+
+while (txt <= 'z')
+{
+_putchar(txt);
+txt++;
+}
+
+_putchar('\n');
+}
