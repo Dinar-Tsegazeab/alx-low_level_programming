ae12dcc4d03aefd05bd33ef8ac4d23ddb6b57d56
Dinar-Tsegazeab <dinaratatsegazeab@gmail.com>
2023-03-21 15:41:43 -0700

    You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+   *_abs - computes the absolute value of a number
+   *@n: The num to be computed
+   *
+   *Return: This function computed +ve int number
+   */
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (n * -1);
+}
+}
+
