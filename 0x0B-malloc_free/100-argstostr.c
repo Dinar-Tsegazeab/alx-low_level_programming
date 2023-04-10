3effd55a9d5566e140ba4ddeb9e0df68f77f7e0a
Dinar-Tsegazeab <dinaratatsegazeab@gmail.com>
2023-04-10 15:21:03 -0700

    It is not the mountains ahead to climb that wear you out

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,37 +7,33 @@
  * @ac: argument count
  * @av: argument vector
  *
- * Return: ...
+ * Return: A pointer to the concatenated string
+ * NULL if ac == 0 or av == NULL or if memory allocation fails
  */
-#include <stdlib.h>
-#include <string.h>
 
 char *argstostr(int ac, char **av)
 {
-int i, j, len;
+int i, j, len = 0, ch = 0;
 char *str;
 
 if (ac == 0 || av == NULL)
 return (NULL);
-
-len = 0;
 for (i = 0; i < ac; i++)
-len += strlen(av[i]) + 1;
-
-str = malloc(sizeof(char) * len);
+{
+for (j = 0; av[i][j]; j++)
+len++;
+ch += len + 1;
+len = 0;
+}
+str = malloc(sizeof(char) * ch + 1);
 if (str == NULL)
 return (NULL);
-
 len = 0;
 for (i = 0; i < ac; i++)
 {
 for (j = 0; av[i][j]; j++)
-{
-str[len] = av[i][j];
-len++;
-}
-str[len] = '\n';
-len++;
+str[len++] = av[i][j];
+str[len++] = '\n';
 }
 str[len] = '\0';
 
