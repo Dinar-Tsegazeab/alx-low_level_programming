8dcd498156f510c26af50b001b708954b6a64bf9
Dinar-Tsegazeab <dinaratatsegazeab@gmail.com>
2023-04-13 05:59:14 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,58 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
+#define ERR_MSG "Error"
 /**
- * multiply - multiplies two numbers
- * @num1: the first number
- * @num2: the second number
+ * is_digit - checik if a string contains a non-digit char
+ * @s: string to be evaluate
  *
- * Return: the result of multiplying num1 and num2
+ * Return: 0 if a non-digit is found, otherwise 1
  */
-int multiply(int num1, int num2)
+int is_digit(char *s)
 {
-return (num1 * num2);
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 /**
- * is_positive - checks if a number is positive
- * @num: the number to check
+ * _strlen - return the lengthof a string
+ * @s: String to evaluate
  *
- * Return: 1 if the number is positive, 0 otherwise
+ * Return: The length of the string
  */
-int is_positive(char *num)
+int _strlen(char *s)
 {
-int i;
-for (i = 0; num[i] != '\0'; i++)
-{
-if (num[i] < '0' || num[i] > '9')
-{
-return (0);
-}
-}
-return (1);
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 /**
- * main - multiplies two positive numbers
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+		exit(98);
+}
+/**
+ *  main - multiplies two positive numbers
  * @argc: the number of arguments
- * @argv: the arguments
+ * @argv: array of  arguments
  *
  * Return: 0 on success, 98 on error
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int num1, num2, result;
-if (argc != 3)
+char *s1, *s2;
+int L1 = 0, L2 = 0, L = 0, i = 0, C = 0, D1 = 0, D2 = 0, *R = 0, a = 0;
+
+s1 = argv[1], s2 = argv[2];
+if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+errors();
+L1 = _strlen(s1);
+L2 = _strlen(s2);
+L = L1 + L2 + 1;
+R = malloc(sizeof(int) * L);
+if (!R)
+return (1);
+for (i = 0; i <= L1 + L2; i++)
+R[i] = 0;
+for (L1 = L1 - 1; L1 >= 0; L1--)
 {
-printf("Error\n");
-return (98);
+D1 = s1[L1] - '0';
+C = 0;
+for (L2 = _strlen(s2) - 1; L2 >= 0; L2--)
+{
+D2 = s2[L2] - '0';
+C += R[L1 + L2 + 1] + (D1 *D2);
+R[L1 + L2 + 1] = C % 10;
+C /= 10;
 }
-if (!is_positive(argv[1]) || !is_positive(argv[2]))
-{
-printf("Error\n");
-return (98);
+if (C > 0)
+R[L1 + L2 + 1] += C;
 }
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = multiply(num1, num2);
-printf("%d\n", result);
+for (i = 0; i < L - 1; i++)
+{
+if (R[i])
+a = 1;
+if (a)
+putchar(R[i] + '0');
+}
+if (!a)
+putchar('0');
+putchar('\n');
+free(R);
 return (0);
 }
